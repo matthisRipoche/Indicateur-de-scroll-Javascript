02471070be9566998ad92f219961cb31cb3d8428
matthisRipoche <matthisripoche@gmail.com>
2024-02-28 11:42:57 +0100

    ok

--- main.js
+++ main.js
@@ -4,4 +4,14 @@ window.onscroll = function () {
 
 function handleScroll() {
   //   Code
+  const bar = document.getElementById("bar");
+  window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY + 238;
+    let minScroll = 0;
+    let maxScroll = 762;
+    const scrollAdvancement = scroll/maxScroll * 100 + "%";
+    console.log(scrollAdvancement);
+    bar.style.width = scrollAdvancement;
+  });
+
 }
